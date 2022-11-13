44902615cdf33938589b665ddaf4fa395ce2daef
osawa-koki <github@osawa.dev>
2022-11-13 20:20:25 +0900

    コードスタイルの調整。

--- practice-log4net.cs/Program.cs
+++ practice-log4net.cs/Program.cs
@@ -8,22 +8,22 @@ using System.Threading.Tasks;
 
 namespace practice_log4net.cs
 {
-    internal class Program
+  internal class Program
+  {
+    private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+    static void Main(string[] args)
     {
-        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-        static void Main(string[] args)
-        {
 
-            var logger = LogManager.GetLogger(Assembly.GetExecutingAssembly().FullName);
+      var logger = LogManager.GetLogger(Assembly.GetExecutingAssembly().FullName);
 
-            // ログ出力
-            logger.Debug("デバッグ：処理の中間情報");
-            logger.Info("情報：操作履歴");
-            logger.Warn("注意：現在は正常に動作が続けられるがデータ不整合の可能性がある操作。");
-            logger.Error("エラー：復旧可能な障害。継続できるが処理はロールバックされる。");
-            logger.Fatal("障害：アプリケーションが強制終了する障害");
+      // ログ出力
+      logger.Debug("デバッグ：処理の中間情報");
+      logger.Info("情報：操作履歴");
+      logger.Warn("注意：現在は正常に動作が続けられるがデータ不整合の可能性がある操作。");
+      logger.Error("エラー：復旧可能な障害。継続できるが処理はロールバックされる。");
+      logger.Fatal("障害：アプリケーションが強制終了する障害");
 
-            Console.ReadLine();
-        }
+      Console.ReadLine();
     }
+  }
 }
