31bb66049796b049c0f8bcd7764b40e8db360600
osawa-koki <github@osawa.dev>
2022-11-13 20:18:42 +0900

    大枠は完成した。

--- practice-log4net.cs/Program.cs
+++ practice-log4net.cs/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,20 @@ namespace practice_log4net.cs
 {
     internal class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
+
+            var logger = LogManager.GetLogger(Assembly.GetExecutingAssembly().FullName);
+
+            // ログ出力
+            logger.Debug("デバッグ：処理の中間情報");
+            logger.Info("情報：操作履歴");
+            logger.Warn("注意：現在は正常に動作が続けられるがデータ不整合の可能性がある操作。");
+            logger.Error("エラー：復旧可能な障害。継続できるが処理はロールバックされる。");
+            logger.Fatal("障害：アプリケーションが強制終了する障害");
+
+            Console.ReadLine();
         }
     }
 }
